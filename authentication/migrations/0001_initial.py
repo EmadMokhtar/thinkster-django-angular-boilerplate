84322167db4ec51016d89be45a154a1e6209d7e8
Emad Mokhtar <emad.m.habib@gmail.com>
2015-10-07 12:48:45 +0300

    Fix created_at field typo

--- authentication/migrations/0001_initial.py
+++ authentication/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('last_name', models.CharField(max_length=40, blank=True)),
                 ('tagline', models.CharField(max_length=140, blank=True)),
                 ('is_admin', models.BooleanField(default=False)),
-                ('create_at', models.DateTimeField(auto_now_add=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
             ],
             options={
--- authentication/models.py
+++ authentication/models.py
@@ -35,7 +35,7 @@ class Account(AbstractBaseUser):
 
     is_admin = models.BooleanField(default=False)
 
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     objects = AccountManager()
